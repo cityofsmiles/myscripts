35b2f9224b5cbc7f1c06ae828ebf91c6a03bf160
cityofsmiles <cityofsmiles@gmail.com>
2021-02-22 11:13:42 +0800

    'edited bashrc'

---
+++ bash-scripts/cp-configs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/sh
+
+cp -u /home/jonathan/.i3/config /home/jonathan/Documents/myscripts/configs/i3-config; 
+
+cp -u /home/jonathan/.bashrc /home/jonathan/Documents/myscripts/configs/bashrc; 
+
+cp -u /home/jonathan/.config/i3status/config /home/jonathan/Documents/myscripts/configs/i3status-config; 
+
+cp -u /home/jonathan/.config/ranger/rifle.conf /home/jonathan/Documents/myscripts/configs; 
+
+cp -u /home/jonathan/.Xmodmap /home/jonathan/Documents/myscripts/configs/Xmodmap; 
+
+cp -u /home/jonathan/.config/ranger/rc.conf /home/jonathan/Documents/myscripts/configs
--- bash-scripts/git-add-commit-push-main.sh
+++ bash-scripts/git-add-commit-push-main.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/sh
 
+cd /home/jonathan/Documents/myscripts
 	git add --all
 	git commit -m "'$1'"
 	git push -u origin main
